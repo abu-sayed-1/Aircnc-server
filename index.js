41f8805652bb0a8041d11fab41be8163df548a19
as ahmed <rakibasrr13@gmail.com>
2021-03-06 08:23:06 +0300

    connect mongodb

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const express = require('express'),
-// const bodyParser = require('body-parser');
+    bodyParser = require('body-parser');
 const cors = require('cors');
-// const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
-
 const app = express();
-const port = 4000;
 app.use(cors());
-// app.use(bodyParser.json);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+const port = 4000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uj2jz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+    const homePageData = client.db(process.env.DB_NAME).collection("homePageData");
+
+
+
+    
+});
+
 
 
 app.get("/", (req, res) => {
-    res.send('hello wold...')
+    res.send('Hello wold ,node working... ')
 })
-
-
-
-
 
 app.listen(port);
