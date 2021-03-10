c4e89b05f5a73d5473e0036db70b71271290adf5
as ahmed <rakibasrr13@gmail.com>
2021-03-10 15:46:50 +0300

    set rooms info

--- index.js
+++ index.js
@@ -14,17 +14,18 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 client.connect(err => {
     const homePageData = client.db(process.env.DB_NAME).collection("homePageData");
-
+    const membersAndDate = client.db(process.env.DB_NAME).collection("membersAndDate");
+    const roomsInfo = client.db(process.env.DB_NAME).collection("roomsInfo");
+    //#home|| post home pages All data
     app.post('/homePagesAllData', (req, res) => {
         const homeData = req.body;
-        console.log(homeData, '[homeData]');
         homePageData.insertMany(homeData)
             .then(result => {
                 res.send(result.insertedCount > 0);
             })
     })
 
-    // get the user-specific destination 
+    //#home||get the user-specific destination 
     app.get("/destination:name", (req, res) => {
         const destinationName = req.params.name;
         const convert = destinationName.toLowerCase();
@@ -35,6 +36,34 @@ client.connect(err => {
                 res.send(document);
             });
     });
+
+    //#home|| post members and startAnd date
+    app.post('/membersAndDates', (req, res) => {
+        const membersAndDatesData = req.body;
+        console.log(membersAndDatesData)
+        membersAndDate.insertOne(membersAndDatesData)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            });
+
+    });
+    // #SelectRoom|| post Rooms Info 
+    app.post('/roomsInfo', (req, res) => {
+        const rooms = req.body;
+        roomsInfo.insertMany(rooms)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            });
+    });
+
+    // #SelectRoom|| get Room specific data
+    app.get('/roomsByData:city', (req, res) => {
+        const room = req.params.city;
+        roomsInfo.find({ "city": room })
+            .toArray((err, document) => {
+                res.send(document);
+            })
+    })
 
 
 });
